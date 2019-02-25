3ac464ca41abfbb369295ed93390a1bb0b891277
latimerb <latimerb@missouri.edu>
2019-02-25 14:20:22 -0600

    condensed notebook

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 from pandas import DataFrame
 from pandas import concat
+from keras.models import Sequential
+from keras.layers import Dropout, LSTM, Dense, Activation
+import numpy as np
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	"""
@@ -33,3 +36,36 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	if dropnan:
 		agg.dropna(inplace=True)
 	return agg
+
+def fit_lstm(train, n_lag, n_batch, nb_epoch, n_neurons):
+    # reshape training into [samples, timesteps, features]
+    X, y = train[:, 0:-n_lag], train[:, -n_lag:]
+    X = X.reshape(X.shape[0], 1, X.shape[1])
+    # define parameters
+    verbose, epochs, batch_size = 1, nb_epoch, n_batch
+    n_timesteps, n_features, n_outputs = X.shape[1], X.shape[2], y.shape[1]
+    # design network
+    model = Sequential()
+    model.add(LSTM(200, activation='relu', input_shape=(n_timesteps, n_features)))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(n_outputs))
+    model.compile(loss='mse', optimizer='adam')
+    # fit network
+    model.fit(X, y, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    return model
+
+def forecast_lstm(model, X, n_batch):
+    # reshape input pattern to [samples, timesteps, features]
+    X = X.reshape(1, 1, len(X))
+    # make forecast
+    forecast = model.predict(X, batch_size=n_batch)
+    # convert to array
+    return forecast
+
+def make_forecasts(model, n_batch, test, n_lag):
+    forecasts = np.zeros((test.shape[0],n_lag))
+    for i in range(test.shape[0]):
+        X, y = test[i, 0:-n_lag], test[i, -n_lag:]
+        # make forecast
+        forecasts[i,:] = forecast_lstm(model, X, n_batch)
+    return forecasts
